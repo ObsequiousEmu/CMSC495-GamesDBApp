1478e6a94de6ce6be07841288f7afaad32402708
Dan <Dan@DESKTOP-BLJODVI>
2017-01-19 18:24:20 -0500

    Add project files.

---
+++ src/GamesDBApp/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesDBApp.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Developer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        
+    }
+}
