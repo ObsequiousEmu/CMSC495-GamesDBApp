7aae49420095f885e257f370f729f37959664172
Dan <Dan@DESKTOP-BLJODVI>
2017-01-27 20:14:31 -0500

    Improved database validation

--- src/GamesDBApp/Models/Game.cs
+++ src/GamesDBApp/Models/Game.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GamesDBApp.Models
 {
     public class Game
     {
         public int GameId { get; set; }
+        [Required]
+        [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z0-9''-'\s]*$", ErrorMessage = "Title must contain only alphanumeric characters.")]
         public string Title { get; set; }
+        [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z0-9''-'\s]*$", ErrorMessage = "Genre must contain only alphanumeric characters.")]
         public string Genre { get; set; }
+        [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z0-9''-'\s]*$", ErrorMessage = "Developer must contain only alphanumeric characters.")]
         public string Developer { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name="Release Date")]
         public DateTime ReleaseDate { get; set; }
 
         
