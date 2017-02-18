e727c1c0295eca276049ac2a9f8dd90fb4d6fdc1
Dan <Dan@DESKTOP-BLJODVI>
2017-02-17 20:25:30 -0500

    AddAuthmessageSender

--- src/GamesDBApp/Services/AuthMessageSenderOptions.cs
+++ src/GamesDBApp/Services/AuthMessageSenderOptions.cs
@@ -8,7 +8,7 @@ namespace GamesDBApp.Services
     public class AuthMessageSenderOptions
     {
         public string SendGridUser { get; set; }
-        public string SendGridKey { get; set; }
+        public string SendGridKey { get; set; } 
     }
 
 
